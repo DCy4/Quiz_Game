b6831b5950e153cd19821e304e05ce80ab2bde3f
DCypher <103863997+DCy4@users.noreply.github.com>
2023-02-13 19:15:17 +0000

    version 1.0 of QG

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from quiz_brain import QuizBrain
 
 
 question_bank = []
-
 for question in question_data:
   question_text = question["text"]
   question_answer = question["answer"]
@@ -12,5 +11,9 @@ for question in question_data:
   question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
-  
+
+while quiz.still_has_questions():
+  quiz.next_question()
+
+print("You have completed the quiz!🎉✨🎊")
+print(f"Your final score: {quiz.score}/ {quiz.question_number} ")
